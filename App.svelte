fb9b618114372ffc8d6c41067db8838d5a777c1d
gio52 <46489862+gio52@users.noreply.github.com>
2020-07-26 21:43:30 +0100

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,19 @@
+<script>
+import Header from "./Header.svelte";
+import global from "./global.css";
+</script>
+<Header/>
+<main>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+ Ut similique eaque dolorum expedita 
+molestias praesentium ex excepturi obcaecati nulla enim?</p>
+</main>
+
+<style>
+main {
+  max-width: 960px;
+  margin: 40px auto;
+}
+
+</style>
+
---
+++ Header.svelte
@@ -0,0 +1,16 @@
+<script>
+import Svg from "./Svg.svelte"
+</script>
+<div class="image">
+<Svg/>
+</div>
+
+<header>
+<h1>
+</h1>
+</header>
+
+<style>
+
+</style>
+
